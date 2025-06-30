302cab2a7295a06b16b78b84df7807ca3b028f48
Samuel Ogwuche <samogwuche98@gmail.com>
2025-06-30 15:36:29 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const date = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright {date}. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
